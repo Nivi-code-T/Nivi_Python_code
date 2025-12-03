d5dc09e7639d4345a14cede5c936b8e1ddd910b9
Nivi-code-T <niveteggi@gmail.com>
2025-12-02 18:27:56 -0600

    Added Day6 Python Code Practice

---
+++ Pythoncode_with_projects/Day6.py
@@ -0,0 +1,20 @@
+#about while loop
+#while condition is True:
+    #Do this
+    #Then Do This
+    #Ten Do This
+count=0
+count_list=""
+while count < 5:
+    print(f"{count}")
+    count +=1
+
+text="hello"
+index=0
+while index < len(text):
+    print(text[index])
+    index+=1
+
+
+
+
